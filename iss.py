bc2ca953529bdf8bf0312f576a88c10b2255b06d
BethanyFolino <bethany.folino@gmail.com>
2021-07-04 23:23:27 -0700

    laid out functions and began trying to make requests and get specified information

--- iss.py
+++ iss.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Bethany Folino'
+
+import requests
 
 
-def main():
-    pass
+def astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    print(r.json())
+
+
+def station_location():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    print(r.text)
+
+
+def over_indy():
+    r = requests.get('http://api.open-notify.org/iss-pass.json')
+    print(r.text)
 
 
 if __name__ == '__main__':
-    main()
+    astronauts()
+    station_location()
+    over_indy()
